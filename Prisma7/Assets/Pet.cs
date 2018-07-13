e84958592c35e49235c1b24034bf8e3b897cc90a
pontura <pontura@gmail.com>
2018-07-13 13:17:49 -0300

    nueva iris con normales bien

---
+++ Prisma7/Assets/Pet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour {
+
+	public float smooth;
+	public Vector3 offset;
+	public Character character;
+
+	void Update () {
+		Vector3 newPos = character.transform.localPosition;
+		transform.localPosition =Vector3.Lerp (transform.localPosition, newPos + offset, smooth);
+	}
+}
