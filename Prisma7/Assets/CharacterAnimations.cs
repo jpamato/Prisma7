e9dff3a9888c382ab3c3abfee792742792b71ea2
jpamato <juanpamato@gmail.com>
2018-08-24 16:32:54 -0300

    sfx world

--- Prisma7/Assets/CharacterAnimations.cs
+++ Prisma7/Assets/CharacterAnimations.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CharacterAnimations : MonoBehaviour {
 
 	public Animation anim;
+	public AudioClip walkSfx;
 
 	public states state;
+
+	AudioSource asource;
 
 	public enum states
 	{
@@ -16,21 +19,37 @@ public class CharacterAnimations : MonoBehaviour {
 	}
 	void Start()
 	{
+		asource = GetComponent<AudioSource> ();
 		Idle ();
 	}
 	public void Idle () {
+		//audio
+		asource.Stop ();
+
 		anim.Play ("idle2");
 		state = states.IDLE;
 	}
 	public void Walk () {
+		//audio
+		asource.Stop ();
+		asource.clip = walkSfx;
+		asource.loop = true;
+		asource.Play ();
+
 		anim.Play ("walk");
 		state = states.WALK;
 	}
 	public void Enter () {
+		//audio
+		asource.Stop ();
+
 		anim.Play ("enter");
 		state = states.ENTER;
 	}
 	public void Cheer () {
+		//audio
+		asource.Stop ();
+
 		anim.Play ("festejo");
 	}
 
