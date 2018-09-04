264a632060bfdcaf0b37596a157467b809a80c22
jpamato <juanpamato@gmail.com>
2018-09-04 18:15:50 -0300

    change MG bg by color level

---
+++ Prisma7/Assets/MateGameBG.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MateGameBG : MonoBehaviour {
+
+	public List<GameObject> backgrounds;
+
+	public void Start(){
+		SetBackground (Data.Instance.levelsData.actualDiamondLevel);
+	}
+
+	public void SetBackground(int index){
+		for (int i = 0; i < backgrounds.Count; i++)
+			backgrounds [i].SetActive (i == index);
+	}
+}
