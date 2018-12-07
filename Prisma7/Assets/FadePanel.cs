a03ecc5beeff4d2db72417df7e963b53070bf9d8
Agustin Pontura <pontura@gmail.com>
2018-12-07 13:55:20 -0300

    loading

--- Prisma7/Assets/FadePanel.cs
+++ Prisma7/Assets/FadePanel.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadePanel : MonoBehaviour {
 
 	public GameObject panel;
 	public Animator anim;
+	public Image bar; 
+	float timer = 0.02f;
 
 	void Start () {
 		panel.SetActive (false);
 	}
 	public void SetOn () {
 		StartCoroutine (Transition ());
+		bar.fillAmount = 0;
 	}
 	IEnumerator Transition() {
 		panel.SetActive (true);
 		anim.Play ("loadingScreenOn");
-		yield return new WaitForSeconds (2);
+		yield return new WaitForSeconds (0.35f);
+		float i = 0;
+		while (i < 1) {
+			i += timer;
+			yield return new WaitForSeconds(timer);
+			bar.fillAmount = i;
+		}
+		bar.fillAmount = 1;
 		anim.Play ("loadingScreenOff");
 		yield return new WaitForSeconds (1);
+
 		panel.SetActive (false);
 	}
 }
