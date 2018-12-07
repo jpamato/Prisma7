6ed2f4da45a2623de3449dc1a501987533654718
Agustin Pontura <pontura@gmail.com>
2018-12-07 12:18:13 -0300

    splash

---
+++ Prisma7/Assets/Splash.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour {
+
+	public void SplashClicked()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("CharacterSelector");
+	}
+}
