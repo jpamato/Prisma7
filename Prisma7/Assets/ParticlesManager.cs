68b1ed6b5dd8f00bfb7875edb1245041e7b15ec5
Agustin Pontura <pontura@gmail.com>
2018-12-13 14:47:47 -0300

    brujo animado

--- Prisma7/Assets/ParticlesManager.cs
+++ Prisma7/Assets/ParticlesManager.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class ParticlesManager : MonoBehaviour {
 
-	public GameObject bubbles;
+	public GameObject[] bubbles;
 	public GameObject bomb;
+	public int id = 0;
 
 	public void SetOn (Fruit.types fruitType, Vector3 pos) {
 		
 		GameObject go = null;
 		switch (fruitType) {
 		case Fruit.types.BAD:
+			id = 0;
 			go = bomb;
 			break;
-		case Fruit.types.GENERIC:
-			go = bubbles;
+		case Fruit.types.GENERIC:			
+			if (id > 3)
+				id = 3;
+			go = bubbles[id];
+			id++;
 			break;
 		}
 
